f84a489be0f9239fad0bf0c7081d8fb6f0405269
SnackZ <rps.ebe.29@gmail.com>
2017-05-14 21:29:36 +0200

    .

---
+++ clan1.php
@@ -0,0 +1,33 @@
+<?
+require_once('Layout.php');
+
+$layout = new Layout();
+
+$layout->kopf(1);
+?>
+Welcome to the District Forsaken!
+<br>
+<br>
+We are a little community of gamer that play primarily CS:GO and PUBG. We own some steam community groups, What’s App group, TeamSpeak3 and in the future a HvH CS:GO server moreover a private CS:GO hack.
+<br>
+<br>
+If you are interested in joining our community, just send to one of the qualified Admins a friendship request.
+You can find all information at Contact.
+	<br>
+	<br>
+	Join District Forsaken at steam:
+	<br>
+	<br>
+	- <a target="_blank" href="http://steamcommunity.com/groups/districtoftheforsaken">District Forsaken Public</a>
+	<br>
+	<br>
+	- <a target="_blank" href="http://steamcommunity.com/groups/DistrictForsaken">District Forsaken Private</a>
+	<br>
+	<br>
+	Subscribe our community at YouTube:
+	<br>
+	<br>
+	- <a target="_blank" href="https://www.youtube.com/channel/UCYmbmz2irdhEJNEseuWZsQQ">YouTube Channel</a>
+
+<?
+$layout->fuss();
