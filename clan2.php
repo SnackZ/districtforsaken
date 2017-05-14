58416f8c1c368a18cb8de4c43fd10c3e79e32e62
SnackZ <rps.ebe.29@gmail.com>
2017-05-14 21:29:36 +0200

    .

---
+++ clan2.php
@@ -0,0 +1,15 @@
+<?
+require_once('Layout.php');
+
+$layout = new Layout();
+
+$layout->kopf(2);
+?>
+
+
+und hier ist Seite 2!
+Für fertige seite bitte bis nächstes wochenende warten!
+
+
+<?
+$layout->fuss();
