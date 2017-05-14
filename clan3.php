88039bd4c1a79434a268f03d3b7d008fafbf7292
SnackZ <rps.ebe.29@gmail.com>
2017-05-14 21:29:36 +0200

    .

---
+++ clan3.php
@@ -0,0 +1,17 @@
+<?
+require_once('Layout.php');
+
+$layout = new Layout();
+
+$layout->kopf(3);
+?>
+
+
+und hier ist die seite 3 und alle freuen sich weil sie gleich auf den link hier klicken: 
+<a target="_blank" href="http://youporn.com">hier klicken!</a>
+<br>
+wenn euch der erste link nicht gefallen hat könnt ihr auch mal hier draufklicken:
+<a target="_blank" href="http://beeg.com">hier klicken!</a>
+
+<?
+$layout->fuss();
